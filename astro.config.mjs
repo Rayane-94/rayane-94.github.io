ec9f9596f3d6c2bbdd6b8a3fc5fe945683ed4830
Rayane Besrour <rayanebesrour3@gmail.com>
2024-04-21 13:09:39 +0200

    Ajout du fichier de configuration de déploiment sur git

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+site: 'https://astro-portfolio.github.io',
+base: 'astro-portfolio',
+
+})
