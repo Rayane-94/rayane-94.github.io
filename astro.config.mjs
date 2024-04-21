a2eacddafaffd1671c6f559c1558284cdc75e8f1
Rayane Besrour <rayanebesrour3@gmail.com>
2024-04-21 13:43:21 +0200

    test deploiement 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-site: 'https://astro-portfolio.github.io',
-base: 'my-repo',
+site: 'https://Rayane-94.github.io',
+base: 'astro-portfolio',
 
 })
