2249bbfe1dd10695cab357cd84ad264f372ee974
Rayane Besrour <rayanebesrour3@gmail.com>
2024-04-24 00:10:34 +0200

    Modification page a propos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-
-site: 'https://Rayane-94.github.io',
-})
+  site: 'https://Rayane-94.github.io',
+  integrations: [icon()]
+});
