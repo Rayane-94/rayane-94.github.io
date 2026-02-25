dedca67f3a189e2f7b2c4fdb637145c782d220ab
Rayane Besrour <rayanebesrour3@gmail.com>
2026-02-25 13:27:24 +0100

    feat config seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import icon from "astro-icon";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Rayane-94.github.io',
-  integrations: [icon()]
+  integrations: [icon(), sitemap()]
 });
